59a1739c8547b05399889c94647720c258e7ce05
Andrew Parelius <parelius@uw.edu>
2015-11-07 10:16:45 -0500

    testing key stats data

--- src/KeyStatsDatabaseBuilder.java
+++ src/KeyStatsDatabaseBuilder.java
@@ -1,19 +1,26 @@
 import harlequinmettle.investmentadviserengine.util.NetworkDownloadTool;
 import harlequinmettle.investmentadviserengine.util.NumberTool;
+import harlequinmettle.utils.debugtools.InstanceCounter;
 import harlequinmettle.utils.filetools.FileTools;
+import harlequinmettle.utils.filetools.SerializationTool;
 import harlequinmettle.utils.finance.updatedtickerset.DatabaseCore;
 import harlequinmettle.utils.stringtools.StringTools;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 // Oct 29, 2015 9:27:19 AM
 
-public class DownloadKeyStats {
+public class KeyStatsDatabaseBuilder {
 
 	static String alternateFiles = "/home/andrew/Desktop/programming";
+
+	static TreeMap<String, HashMap<String, Float>> database = new TreeMap<String, HashMap<String, Float>>();
 
 	public static void main(String[] args) {
 		// Oct 29, 2015 9:27:19 AM
@@ -23,7 +30,84 @@ public class DownloadKeyStats {
 		// new DownloadKeyStats().extractTableData();
 		// extractDataFromTables();
 		buildDatabaseFromExtractedTables();
-		// putAllExtractedTablesInOneFileForExamination();
+		testDatabaseValues();
+	}
+
+	// Nov 7, 2015 9:24:33 AM
+	private static void testDatabaseValues() {
+		database = SerializationTool.deserializeObject(database.getClass(), "obj_database_key_stats");
+		InstanceCounter dataQuantitycounter = new InstanceCounter();
+		InstanceCounter dataQuaalitycounter = new InstanceCounter();
+		for (Entry<String, HashMap<String, Float>> ent : database.entrySet()) {
+			String ticker = ent.getKey();
+			HashMap<String, Float> data = ent.getValue();
+			dataQuantitycounter.add(data.size());
+			for (Entry<String, Float> keystats : data.entrySet()) {
+				dataQuantitycounter.add(keystats.getKey());
+				dataQuaalitycounter.add(keystats.getValue());
+			}
+		}
+		for (Entry<Float, Integer> ent : dataQuantitycounter.FLOAT_COUNTER.entrySet()) {
+			System.out.println(ent.getKey() + "      " + ent.getValue());
+		}
+		for (Entry<String, Integer> ent : dataQuantitycounter.STRING_COUNTER.entrySet()) {
+			System.out.println(ent.getKey() + "      " + ent.getValue());
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println(" NaN " + dataQuaalitycounter.FLOAT_COUNTER.get(Float.NaN));
+		System.out.println(" zero " + dataQuaalitycounter.FLOAT_COUNTER.get(0f));
+		System.out.println(" +inf " + dataQuaalitycounter.FLOAT_COUNTER.get(Float.POSITIVE_INFINITY));
+		System.out.println(" -inf " + dataQuaalitycounter.FLOAT_COUNTER.get(Float.NEGATIVE_INFINITY));
+		// for (Entry<Float, Integer> ent :
+		// dataQuaalitycounter.FLOAT_COUNTER.entrySet()) {
+		// System.out.println(ent.getValue() +
+		// "                                   " + ent.getKey());
+		// }
+	}
+
+	// Nov 7, 2015 8:59:55 AM
+	private static void buildDatabaseFromExtractedTables() {
+
+		DatabaseCore tickerData = DatabaseCore.getDataCoreSingleton();
+		int counter = 0;
+		for (String ticker : tickerData.tickers.values()) {
+			System.out.println(counter++ + "    " + ticker);
+			File keyStatsFileExtract = new File(alternateFiles, "extracted" + File.separatorChar + ticker + "tables.html");
+			String htmlTale = FileTools.tryToReadFileToString(keyStatsFileExtract, null);
+			if (htmlTale == null)
+				continue;
+			String[] trs = htmlTale.split("<tr.*?>");
+			for (String tr : trs) {
+				String[] tds = tr.split("</td>");
+				if (tds.length < 3) {
+					continue;
+				}
+				String key = StringTools.replaceAllRegex(tds[0], "<.*?>", "");
+				if (key.length() > 45)
+					continue;
+				key = key.replaceAll("\\(.*?,.*?\\)", "");
+				String value = StringTools.replaceAllRegex(tds[1], "<.*?>", "");
+				float f = NumberTool.stringNumberWithMBKtoFloat(value);
+				if (f != f) {
+					continue;
+				}
+				float dataValue = (float) (NumberTool.roundToSignificantFigures2(f, f));
+				addData(ticker, key, dataValue);
+			}
+		}
+
+		SerializationTool.serializeObject(database, "obj_database_key_stats");
+	}
+
+	private static void addData(String ticker, String key, float value) {
+		// Nov 7, 2015 9:18:10 AM
+		HashMap<String, Float> keyStats = database.get(ticker);
+		if (keyStats == null)
+			keyStats = new HashMap<String, Float>();
+		keyStats.put(key, value);
+		database.put(ticker, keyStats);
+
 	}
 
 	private static void extractDataFromTables() {
@@ -70,22 +154,6 @@ public class DownloadKeyStats {
 		FileTools.tryToWriteCollectionToFileLines(new File(alternateFiles, "dataextract_failuresnumber.txt"), numberfailures);
 		System.out.println(" success " + tdsuccess);
 		System.out.println("     fail " + tdfail);
-	}
-
-	// Nov 2, 2015 9:46:40 AM
-	private static void putAllExtractedTablesInOneFileForExamination() {
-		String onePage = "<html>";
-		DatabaseCore tickerData = DatabaseCore.getDataCoreSingleton();
-		int counter = 0;
-		for (String ticker : tickerData.tickers.values()) {
-			System.out.println(counter++ + "    " + ticker);
-			File keyStatsFileExtract = new File(alternateFiles, "extracted" + File.separatorChar + ticker + "tables.html");
-			onePage += FileTools.tryToReadFileToString(keyStatsFileExtract, "<P>UNABLE TO EXTRACT FOR " + ticker + "</P><BR>")
-					+ System.lineSeparator();
-
-		}
-		onePage += "</html>";
-		FileTools.tryToWriteStringToFile(new File(alternateFiles, "ONE_BIG_WEB_PAGE.HTML"), onePage);
 	}
 
 	// Oct 29, 2015 10:42:43 AM
