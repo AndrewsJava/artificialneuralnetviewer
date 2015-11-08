dcc429790a6814e834da943f300480df8137466b
Andrew Parelius <parelius@uw.edu>
2015-11-08 11:23:34 -0500

    data

--- src/KeyStatsDatabaseBuilder.java
+++ src/KeyStatsDatabaseBuilder.java
@@ -9,8 +9,12 @@ import harlequinmettle.utils.stringtools.StringTools;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -22,6 +26,7 @@ public class KeyStatsDatabaseBuilder {
 
 	static TreeMap<String, HashMap<String, Float>> database = new TreeMap<String, HashMap<String, Float>>();
 	static TreeMap<String, float[]> inputs = new TreeMap<String, float[]>();
+	static TreeMap<String, int[]> inputsQuality = new TreeMap<String, int[]>();
 
 	public static void main(String[] args) {
 		// Oct 29, 2015 9:27:19 AM
@@ -38,16 +43,93 @@ public class KeyStatsDatabaseBuilder {
 
 	private static void dataQuality() {
 		inputs = SerializationTool.deserializeObject(inputs.getClass(), "obj_database_inputs_key_stats");
-		int[] counts = new int[keysToUse.length];
 
-		for (Entry<String, float[]> keystats : inputs.entrySet()) {
-			int i = 0;
-			for (float f : keystats.getValue()) {
-				if (f == f)
-					counts[i++]++;
-			}
+		System.out.println(keysToUse.length + " , ");
+		TreeSet<Integer> useKeys = new TreeSet<Integer>();
+
+		for (int i = 0; i < keysToUse.length; i++) {
+			useKeys.add(i);
 		}
-		System.out.println(Arrays.toString(counts));
+		Set<Set<Integer>> powerSet = powerSet(useKeys);
+		for (Set<Integer> set : powerSet) {
+
+			for (int i : set)
+				System.out.print(keysToUse[i] + " , ");
+			System.out.println();
+			TreeSet<String> quailityData = new TreeSet<String>();
+			for (Entry<String, float[]> keystats : inputs.entrySet()) {
+				float[] subset = buildSubset(set, keystats.getValue());
+
+				if (Float.isNaN(sum(subset)))
+					continue;
+				quailityData.add(keystats.getKey());
+			}
+			System.out.println(quailityData.size());
+		}
+	}
+
+	private static float[] buildSubset(Set<Integer> set, float[] fullInputSet) {
+		float[] subSet = new float[set.size()];
+		int index = 0;
+		for (int i : set) {
+			subSet[index++] = fullInputSet[i];
+		}
+		return subSet;
+	}
+
+	private static float sum(float[] data) {
+		float sum = 0;
+		for (float f : data) {
+
+			if (f != f)
+				return Float.NaN;
+			sum += f;
+		}
+		return sum;
+	}
+
+	private static float sum(Collection<Integer> useOnly, float[] data) {
+		float sum = 0;
+		for (int i : useOnly) {
+			float f = data[i];
+			if (f != f)
+				return Float.NaN;
+			sum += f;
+		}
+		return sum;
+	}
+
+	public static Set<Set<Integer>> powerSet(Set<Integer> originalSet) {
+		Set<Set<Integer>> sets = new HashSet<Set<Integer>>();
+		if (originalSet.isEmpty()) {
+			sets.add(new HashSet<Integer>());
+			return sets;
+		}
+		List<Integer> list = new ArrayList<Integer>(originalSet);
+		Integer head = list.get(0);
+		Set<Integer> rest = new HashSet<Integer>(list.subList(1, list.size()));
+		for (Set<Integer> set : powerSet(rest)) {
+			Set<Integer> newSet = new HashSet<Integer>();
+			newSet.add(head);
+			newSet.addAll(set);
+			sets.add(newSet);
+			sets.add(set);
+		}
+		return sets;
+	}
+
+	private static float sum(float[] data, Collection<Integer> indexSkip) {
+		float sum = 0;
+		int i = 0;
+		for (float f : data) {
+			if (indexSkip.contains(i++))
+				continue;
+
+			if (f != f)
+				return Float.NaN;
+			sum += f;
+		}
+		return sum;
 	}
 
 	private static void buildInputDataFromKeyStats() {
